1f3a602b595c716ed869d170e9c237726bd55b46
charbs0701 <chaqhdtjr7@gmail.com>
2023-03-13 05:44:37 +0900

    Fix sendVerifyEmail email-auth-type 헤더 추가 회원가입시에는 헤더에 create 값, id 찾기 시에는 헤더에 find 값 받음

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -15,10 +15,22 @@ export const sendVerifyEmail = async (req, res) => {
   // nodemailer 모듈을 사용하여 이메일을 전송합니다.
 
   const { email } = req.body; // 유저의 이메일을 받아옵니다.
+  const emailAuthType = req.headers["email-auth-type"]; // 이메일 인증 타입을 받아옵니다.
 
-  if (await getUserByEmail(email)) {
-    return res.status(409).json({ message: "이미 가입된 이메일입니다." });
+  if (emailAuthType === "create") {
+    // 회원가입 할 때의 이메일 인증
+    if (await getUserByEmail(email)) {
+      return res.status(409).json({ message: "이미 가입된 이메일입니다." });
+    }
+  } else if (emailAuthType === "find") {
+    // 아아디/비밀번호 찾기 할 때의 이메일 인증
+    if (!(await getUserByEmail(email))) {
+      return res.status(409).json({ message: "가입되지 않은 이메일입니다." });
+    }
+  } else {
+    return res.status(400).json({ message: "이메일 인증 타입이 없습니다." });
   }
+
   try {
     const verificationCode = Math.floor(100000 + Math.random() * 900000); // 6자리 인증코드 생성
 
